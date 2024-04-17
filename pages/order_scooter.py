1ec568ac5fe117df96ec865443ea9969714db8c2
Evgeny Krysenkov <eugene.krysenkov@yandex.com>
2024-04-17 19:28:44 +0300

    Создан родительский класс BasePage - там разместятся методы общие для всех страниц

--- pages/order_scooter.py
+++ pages/order_scooter.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-class HomePage:
+class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
@@ -13,47 +13,12 @@ class HomePage:
     def get_site(self, site):
         self.driver.get(site)
 
-    # прокрутка до элемента на странице
-    def scroll_element(self, scroll_element):
-        element = self.driver.find_element(*scroll_element)
-        self.driver.execute_script("arguments[0].scrollIntoView();", element)
-
-    # нажатие на элемент
-    def click_on_section(self, section):
-        self.driver.find_element(*section).click()
-
-    # ожидание до кликабельного элемента
-    def wait_element_clickable(self, element_clickable):
-        WebDriverWait(self.driver, 3).until(
-            expected_conditions.element_to_be_clickable(element_clickable))
-
-
-class OrderPage:
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    # выбор станции метро
-    def selection_metro(self, string_metro, metro):
-        time.sleep(2)
-        self.driver.find_element(*string_metro).send_keys(metro)
-        self.driver.find_element(*string_metro).send_keys(Keys.ARROW_DOWN)
-        self.driver.find_element(*string_metro).send_keys(Keys.ENTER)
-
     # отправка сообщения в строку
     def filling_string(self, string, message):
         self.driver.find_element(*string).send_keys(message)
 
-    # выбор цвета скутера
-    def checkbox_color_scooter(self, color_scooter):
-        self.driver.find_element(*color_scooter).click()
+    # прокрутка до элемента на странице
 
-    # выбор даты
-    def selection_date(self, string_when_bring_scooter, date):
-        self.driver.find_element(*string_when_bring_scooter).send_keys(date)
-        self.driver.find_element(*string_when_bring_scooter).send_keys(Keys.ENTER)
-
-    # прокрутка до элемента
     def scroll_element(self, scroll_element):
         element = self.driver.find_element(*scroll_element)
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
@@ -76,6 +41,28 @@ class OrderPage:
     def get_text_of_element(self, text_of_element):
         text = self.driver.find_element(*text_of_element).text
         return text
+
+
+class OrderPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    # выбор станции метро
+    def selection_metro(self, string_metro, metro):
+        time.sleep(2)
+        self.driver.find_element(*string_metro).send_keys(metro)
+        self.driver.find_element(*string_metro).send_keys(Keys.ARROW_DOWN)
+        self.driver.find_element(*string_metro).send_keys(Keys.ENTER)
+
+    # выбор цвета скутера
+    def checkbox_color_scooter(self, color_scooter):
+        self.driver.find_element(*color_scooter).click()
+
+    # выбор даты
+    def selection_date(self, string_when_bring_scooter, date):
+        self.driver.find_element(*string_when_bring_scooter).send_keys(date)
+        self.driver.find_element(*string_when_bring_scooter).send_keys(Keys.ENTER)
 
     # переключение на новую вкладку
     def switch_new_tab(self):
