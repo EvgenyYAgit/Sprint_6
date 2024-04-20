4ee0cf907c1015582abfdbe104b5315eaae9055f
Evgeny Krysenkov <eugene.krysenkov@yandex.com>
2024-04-20 16:39:34 +0300

    Commit 11 Вернул РО для home page. Создал уникальный метод для test_list_questions.py Остальные общие методы берутся из basepage.

--- tests/test_list_questions.py
+++ tests/test_list_questions.py
@@ -1,4 +1,4 @@
-from pages.order_scooter import OrderPage
+from pages.homepage import HomePage
 import allure
 import locators.list_questions_locators
 from locators.url_base import site
@@ -9,78 +9,77 @@ class TestListQuestions:
 
     @allure.title('Проверка ответа на вопрос: Сколько это стоит? И как оплатить?')
     def test_question_how_much_it_cost(self, start_and_stop_browser):
-        home_page = OrderPage(start_and_stop_browser)
+        home_page = HomePage(start_and_stop_browser)
         home_page.get_site(site)
-        home_page.search_element_and_click(locators.list_questions_locators.question_how_much_it_cost)
-        home_page.wait_element_located(locators.list_questions_locators.answer_how_much_it_cost)
-        answer = home_page.get_text_of_element(locators.list_questions_locators.answer_how_much_it_cost)
+        answer = home_page.search_question_and_open_answer(locators.list_questions_locators.question_how_much_it_cost,
+                                                           locators.list_questions_locators.answer_how_much_it_cost)
         assert answer == 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.'
 
     @allure.title('Проверка ответа на вопрос: Хочу сразу несколько самокатов! Так можно?')
     def test_question_want_several_scooters(self, start_and_stop_browser):
-        home_page = OrderPage(start_and_stop_browser)
+        home_page = HomePage(start_and_stop_browser)
         home_page.get_site(site)
-        home_page.search_element_and_click(locators.list_questions_locators.question_want_several_scooters)
-        home_page.wait_element_located(locators.list_questions_locators.answer_want_several_scooters)
-        answer = home_page.get_text_of_element(locators.list_questions_locators.answer_want_several_scooters)
+        answer = home_page.search_question_and_open_answer(
+            locators.list_questions_locators.question_want_several_scooters,
+            locators.list_questions_locators.answer_want_several_scooters)
         assert answer == ('Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, '
                           'можете просто сделать несколько заказов — один за другим.')
 
     @allure.title('Проверка ответа на вопрос: Как рассчитывается время аренды?')
     def test_question_how_rental_time(self, start_and_stop_browser):
-        home_page = OrderPage(start_and_stop_browser)
+        home_page = HomePage(start_and_stop_browser)
         home_page.get_site(site)
-        home_page.search_element_and_click(locators.list_questions_locators.question_how_rental_time)
-        home_page.wait_element_located(locators.list_questions_locators.answer_how_rental_time)
-        answer = home_page.get_text_of_element(locators.list_questions_locators.answer_how_rental_time)
+        answer = home_page.search_question_and_open_answer(
+            locators.list_questions_locators.question_how_rental_time,
+            locators.list_questions_locators.answer_how_rental_time)
         assert answer == ('Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт '
                           'времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли '
                           'самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.')
 
     @allure.title('Проверка ответа на вопрос: Можно ли заказать самокат прямо на сегодня?')
     def test_question_order_today(self, start_and_stop_browser):
-        home_page = OrderPage(start_and_stop_browser)
+        home_page = HomePage(start_and_stop_browser)
         home_page.get_site(site)
-        home_page.search_element_and_click(locators.list_questions_locators.question_order_today)
-        home_page.wait_element_located(locators.list_questions_locators.answer_order_today)
-        answer = home_page.get_text_of_element(locators.list_questions_locators.answer_order_today)
+        answer = home_page.search_question_and_open_answer(
+            locators.list_questions_locators.question_order_today,
+            locators.list_questions_locators.answer_order_today)
         assert answer == 'Только начиная с завтрашнего дня. Но скоро станем расторопнее.'
 
     @allure.title('Проверка ответа на вопрос: Можно ли продлить заказ или вернуть самокат раньше?')
     def test_question_renew_or_return_scooter(self, start_and_stop_browser):
-        home_page = OrderPage(start_and_stop_browser)
+        home_page = HomePage(start_and_stop_browser)
         home_page.get_site(site)
-        home_page.search_element_and_click(locators.list_questions_locators.question_renew_or_return_scooter)
-        home_page.wait_element_located(locators.list_questions_locators.answer_renew_or_return_scooter)
-        answer = home_page.get_text_of_element(locators.list_questions_locators.answer_renew_or_return_scooter)
+        answer = home_page.search_question_and_open_answer(
+            locators.list_questions_locators.question_renew_or_return_scooter,
+            locators.list_questions_locators.answer_renew_or_return_scooter)
         assert answer == ('Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому '
                           'номеру 1010.')
 
     @allure.title('Проверка ответа на вопрос: Вы привозите зарядку вместе с самокатом?')
     def test_question_charging_with_scooter(self, start_and_stop_browser):
-        home_page = OrderPage(start_and_stop_browser)
+        home_page = HomePage(start_and_stop_browser)
         home_page.get_site(site)
-        home_page.search_element_and_click(locators.list_questions_locators.question_charging_together_scooter)
-        home_page.wait_element_located(locators.list_questions_locators.answer_charging_together_scooter)
-        answer = home_page.get_text_of_element(locators.list_questions_locators.answer_charging_together_scooter)
+        answer = home_page.search_question_and_open_answer(
+            locators.list_questions_locators.question_charging_together_scooter,
+            locators.list_questions_locators.answer_charging_together_scooter)
         assert answer == ('Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете '
                           'кататься без передышек и во сне. Зарядка не понадобится.')
 
     @allure.title('Проверка ответа на вопрос: Можно ли отменить заказ?')
     def test_question_cancel_order(self, start_and_stop_browser):
-        home_page = OrderPage(start_and_stop_browser)
+        home_page = HomePage(start_and_stop_browser)
         home_page.get_site(site)
-        home_page.search_element_and_click(locators.list_questions_locators.question_cancel_order)
-        home_page.wait_element_located(locators.list_questions_locators.answer_cancel_order)
-        answer = home_page.get_text_of_element(locators.list_questions_locators.answer_cancel_order)
+        answer = home_page.search_question_and_open_answer(
+            locators.list_questions_locators.question_cancel_order,
+            locators.list_questions_locators.answer_cancel_order)
         assert answer == ('Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все '
                           'же свои.')
 
     @allure.title('Проверка ответа на вопрос: Я жизу за МКАДом, привезёте?')
     def test_question_about_moscow_ring_road(self, start_and_stop_browser):
-        home_page = OrderPage(start_and_stop_browser)
+        home_page = HomePage(start_and_stop_browser)
         home_page.get_site(site)
-        home_page.search_element_and_click(locators.list_questions_locators.question_about_moscow_ring_road)
-        home_page.wait_element_located(locators.list_questions_locators.answer_about_moscow_ring_road)
-        answer = home_page.get_text_of_element(locators.list_questions_locators.answer_about_moscow_ring_road)
+        answer = home_page.search_question_and_open_answer(
+            locators.list_questions_locators.question_about_moscow_ring_road,
+            locators.list_questions_locators.answer_about_moscow_ring_road)
         assert answer == 'Да, обязательно. Всем самокатов! И Москве, и Московской области.'
